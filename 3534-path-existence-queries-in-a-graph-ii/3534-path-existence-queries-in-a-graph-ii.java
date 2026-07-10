9d388949612574f6d0b45f65cbac80bcc89e207a
SHUBHANKAR MAITY <indiavivo956@gmail.com>
2026-07-10 17:59:50 +0530

    Time: 89 ms (87.5%), Space: 274.4 MB (75%) - LeetHub

---
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+class Solution {
+    public int[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+
+        int[] pos = new int[n];
+        for (int i = 0; i < n; i++) {
+            pos[arr[i][1]] = i;
+        }
+
+        int[] next = new int[n];
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            while (j + 1 < n && arr[j + 1][0] - arr[i][0] <= maxDiff) {
+                j++;
+            }
+            next[i] = j;
+        }
+
+        int LOG = 18;
+        int[][] up = new int[LOG][n];
+
+        for (int i = 0; i < n; i++) {
+            up[0][i] = next[i];
+        }
+
+        for (int k = 1; k < LOG; k++) {
+            for (int i = 0; i < n; i++) {
+                up[k][i] = up[k - 1][up[k - 1][i]];
+            }
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int qi = 0; qi < queries.length; qi++) {
+            int u = pos[queries[qi][0]];
+            int v = pos[queries[qi][1]];
+
+            if (u > v) {
+                int t = u;
+                u = v;
+                v = t;
+            }
+
+            if (u == v) {
+                ans[qi] = 0;
+                continue;
+            }
+
+            if (next[u] == u) {
+                ans[qi] = -1;
+                continue;
+            }
+
+            int cur = u;
+            int steps = 0;
+
+            for (int k = LOG - 1; k >= 0; k--) {
+                if (up[k][cur] < v) {
+                    cur = up[k][cur];
+                    steps += 1 << k;
+                }
+            }
+
+            if (next[cur] >= v) {
+                ans[qi] = steps + 1;
+            } else {
+                ans[qi] = -1;
+            }
+        }
+
+        return ans;
+    }
+}
