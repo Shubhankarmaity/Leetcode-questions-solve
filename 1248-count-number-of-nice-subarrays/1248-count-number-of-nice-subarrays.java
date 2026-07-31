c65cb492ccffc04ad306c1fafd6bfda73bc053bb
SHUBHANKAR MAITY <indiavivo956@gmail.com>
2026-08-01 01:19:20 +0530

    Time: 45 ms (10.23%), Space: 56.5 MB (81.86%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -2,7 +2,6 @@ class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        // We have seen 0 odd numbers once before starting
         map.put(0, 1);
 
         int oddCount = 0;
@@ -10,17 +9,14 @@ class Solution {
 
         for (int num : nums) {
 
-            // Count odd numbers
             if (num % 2 != 0) {
                 oddCount++;
             }
 
-            // Need an earlier prefix with (oddCount - k) odds
             if (map.containsKey(oddCount - k)) {
                 answer += map.get(oddCount - k);
             }
 
-            // Store current prefix count
             map.put(oddCount, map.getOrDefault(oddCount, 0) + 1);
         }
 
