22c39957d629d575541bf2a15befe78a66335d2d
SHUBHANKAR MAITY <indiavivo956@gmail.com>
2026-08-03 23:16:11 +0530

    Time: 10 ms (83.24%), Space: 85.4 MB (76.7%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -2,20 +2,15 @@ class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
 
-        // dp[i] = maximum score difference (Alice - Bob)
-        // starting from index i.
         int[] dp = new int[n + 1];
 
         for (int i = n - 1; i >= 0; i--) {
             int take = 0;
             dp[i] = Integer.MIN_VALUE;
 
-            // Take 1, 2, or 3 stones
             for (int j = i; j < Math.min(i + 3, n); j++) {
                 take += stoneValue[j];
 
-                // After taking stones, opponent gets the turn.
-                // dp[j + 1] is opponent's advantage, so subtract it.
                 dp[i] = Math.max(dp[i], take - dp[j + 1]);
             }
         }
