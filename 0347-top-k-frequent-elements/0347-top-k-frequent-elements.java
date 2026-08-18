ff05cc121bd988cfa109605448fe228aa52e78d0
SHUBHANKAR MAITY <indiavivo956@gmail.com>
2026-08-19 02:28:09 +0530

    Time: 17 ms (19.38%), Space: 47.7 MB (33.46%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,29 +1,17 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> tm = new TreeMap<>();
-
-        // Count frequency
-        for (int i = 0; i < nums.length; i++) {
-            tm.put(nums[i], tm.getOrDefault(nums[i], 0) + 1);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-
-        // Max heap: stores [number, frequency]
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> b[1] - a[1]
-        );
-
-        // Add all elements to heap
-        for (int key : tm.keySet()) {
-            pq.offer(new int[]{key, tm.get(key)});
-        }
-
-        // Get top k frequent elements
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list,(a,b) -> {
+            return map.get(b) - map.get(a);
+        });
         int[] ans = new int[k];
-
-        for (int i = 0; i < k; i++) {
-            ans[i] = pq.poll()[0];
+        for(int i = 0; i< k ;i++){
+            ans[i] = list.get(i);
         }
-
         return ans;
     }
 }
