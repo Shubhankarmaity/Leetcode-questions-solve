d4e4196f120bb8cd6dbab79a7efa712a0d29a524
SHUBHANKAR MAITY <indiavivo956@gmail.com>
2026-08-31 01:58:16 +0530

    Time: 21 ms (34.22%), Space: 54.1 MB (78%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -5,7 +5,6 @@ class Solution {
 
         int[][] dp = new int[m + 1][n + 1];
 
-        // Empty t can always be formed in exactly one way
         for (int i = 0; i <= m; i++) {
             dp[i][0] = 1;
         }
@@ -13,10 +12,8 @@ class Solution {
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
 
-                // Don't take s[i-1]
                 dp[i][j] = dp[i - 1][j];
 
-                // Take s[i-1] if characters match
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
                     dp[i][j] += dp[i - 1][j - 1];
                 }
