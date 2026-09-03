403149a0f2aa4e06dbe9309019b4f52c5e4a8943
SHUBHANKAR MAITY <indiavivo956@gmail.com>
2026-09-03 22:07:25 +0530

    Time: 9 ms (34.78%), Space: 121.8 MB (30.44%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -8,12 +8,10 @@ class Solution {
             }
         }
 
-        // If there is no odd number, array is already uniform
         if (minOdd == Integer.MAX_VALUE) {
             return true;
         }
 
-        // Every even number must be >= minOdd
         for (int num : nums) {
             if (num % 2 == 0 && num < minOdd) {
                 return false;
