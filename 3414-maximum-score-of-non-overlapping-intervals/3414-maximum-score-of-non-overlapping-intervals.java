bc9d9fc68f03c43dd9413b331e9f811818dd2735
SHUBHANKAR MAITY <indiavivo956@gmail.com>
2026-09-13 00:50:14 +0530

    Time: 90 ms (94.44%), Space: 195.1 MB (38.89%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.java
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.java
@@ -15,7 +15,6 @@ class Solution {
     public int[] maximumWeight(List<List<Integer>> intervals) {
         int n = intervals.size();
 
-        // [start, end, weight, originalIndex]
         int[][] a = new int[n][4];
 
         for (int i = 0; i < n; i++) {
@@ -36,16 +35,6 @@ class Solution {
             return Integer.compare(x[3], y[3]);
         });
 
-        /*
-         * prev[i] = number of intervals before i that are compatible
-         * with interval i.
-         *
-         * Since intervals are inclusive:
-         *
-         * previous.end < current.start
-         *
-         * is required.
-         */
         int[] prev = new int[n];
 
         for (int i = 0; i < n; i++) {
@@ -67,11 +56,6 @@ class Solution {
             prev[i] = lo;
         }
 
-        /*
-         * dp[i][k] =
-         * best result using the first i intervals
-         * and choosing at most k intervals.
-         */
         State[][] dp = new State[n + 1][5];
 
         for (int i = 0; i <= n; i++) {
@@ -86,10 +70,8 @@ class Solution {
 
             for (int k = 1; k <= 4; k++) {
 
-                // Option 1: don't take interval idx.
                 State skip = dp[i - 1][k];
 
-                // Option 2: take interval idx.
                 State old = dp[prev[idx]][k - 1];
 
                 int[] ids = new int[old.ids.length + 1];
@@ -104,8 +86,6 @@ class Solution {
                     old.ids.length
                 );
 
-                // Answer must be lexicographically compared by
-                // original indices, so keep them sorted.
                 Arrays.sort(ids);
 
                 State take = new State(
@@ -122,12 +102,10 @@ class Solution {
 
     private State better(State a, State b) {
 
-        // Higher weight wins.
         if (a.score != b.score) {
             return a.score > b.score ? a : b;
         }
 
-        // Same weight -> lexicographically smaller indices.
         return lexicographicallySmaller(a.ids, b.ids) ? a : b;
     }
 
